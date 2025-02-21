2de9ea3bcfdbd7d37e15545a9f279b3df714cd79
DrojasFrontend <daniel.rojas@leggercolombia.com>
2025-02-21 11:51:42 -0500

    cambios

--- page-template-lg/page-ortopedia-cadera.php
+++ page-template-lg/page-ortopedia-cadera.php
@@ -1,6 +1,6 @@
 <?php
 /* 
-Template Name: Plantilla Especialidad | Ortopedia
+Template Name: Plantilla Especialidad | Ortopedia Cadera
 */ 
 
 get_header();
@@ -107,7 +107,7 @@ $nombrePagina = !empty($grupo_tab_tarjeta_texto_imagen['nombre_pagina']) ? esc_h
     <?php 
       get_template_part('template-parts/especialidades/seccion', 'tab-tarjeta-texto-imagen', 
         array(
-          'active' => 'Ortopedia Trauma y recontstrucción',
+          'active' => 'Ortopedia Cadera',
           'especialidad' => $especialidad,
           'nombrePagina' => $nombrePagina
         )
